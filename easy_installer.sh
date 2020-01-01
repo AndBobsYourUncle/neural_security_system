3d4a16b31036353c4e7c18406057c7e95f0c4a75
Nicholas Page <npage85@gmail.com>
2020-01-01 13:32:47 -0800

    finish easy_installer.sh

--- easy_installer.sh
+++ easy_installer.sh
@@ -15,10 +15,13 @@ mkdir -p $TEMP_DIR && cd $TEMP_DIR && \
 wget -c $DOWNLOAD_LINK && \
 tar xf l_openvino_toolkit*.tgz && \
 rm l_openvino_toolkit*.tgz && \
-cd l_openvino_toolkit* && \
+cd l_openvino_toolkit*
+
 sed -i 's/decline/accept/g' silent.cfg && \
-sed -i 's/=DEFAULTS/=intel-openvino-ie-sdk-ubuntu-bionic__x86_64;intel-openvino-ie-rt-cpu-ubuntu-bionic__x86_64;intel-openvino-ie-rt-vpu-ubuntu-bionic__x86_64;intel-openvino-model-optimizer__x86_64;intel-openvino-opencv-lib-ubuntu-bionic__x86_64/g' silent.cfg && \
-./install.sh -s silent.cfg && \
+sed -i 's/=DEFAULTS/=intel-openvino-ie-sdk-ubuntu-bionic__x86_64;intel-openvino-ie-rt-cpu-ubuntu-bionic__x86_64;intel-openvino-ie-rt-vpu-ubuntu-bionic__x86_64;intel-openvino-model-optimizer__x86_64;intel-openvino-opencv-lib-ubuntu-bionic__x86_64/g' silent.cfg
+
+./install.sh -s silent.cfg
+
 rm -rf $TEMP_DIR
 
 $INSTALL_DIR/install_dependencies/install_openvino_dependencies.sh
@@ -48,7 +51,8 @@ cmake-curses-gui && \
 rm -rf /var/lib/apt/lists/*
 
 cd /usr && git clone https://github.com/eclipse/paho.mqtt.c.git && \
-cd /usr/paho.mqtt.c && git checkout v1.3.1 && \
+cd /usr/paho.mqtt.c
+git checkout v1.3.1
 cmake -Bbuild -H. -DPAHO_WITH_SSL=ON -DPAHO_BUILD_SHARED=ON && \
 cmake --build build/ --target install && \
 ldconfig && \
@@ -56,7 +60,8 @@ cd /usr && \
 rm -rf /usr/paho.mqtt.c
 
 cd /usr && git clone https://github.com/eclipse/paho.mqtt.cpp && \
-cd /usr/paho.mqtt.cpp && git checkout v1.1 && \
+cd /usr/paho.mqtt.cpp
+git checkout v1.1
 cmake -Bbuild -H. -DPAHO_WITH_SSL=ON -DPAHO_BUILD_SHARED=ON && \
 cmake --build build/ --target install && \
 ldconfig && \
@@ -64,8 +69,8 @@ cd /usr && \
 rm -rf /usr/paho.mqtt.cpp
 
 cd /usr && git clone https://github.com/jbeder/yaml-cpp.git && \
-cd /usr/yaml-cpp/ && git checkout yaml-cpp-0.6.3 && mkdir build && \
-cd /usr/yaml-cpp/build && \
+cd /usr/yaml-cpp/git checkout yaml-cpp-0.6.3 && mkdir build
+cd /usr/yaml-cpp/build
 cmake -DYAML_BUILD_SHARED_LIBS=ON .. && \
 cmake --build ./ --target install && \
 ldconfig && \
@@ -78,12 +83,12 @@ cd tensorflow-yolo-v3 && \
 git checkout ed60b90 && \
 wget https://raw.githubusercontent.com/pjreddie/darknet/master/data/coco.names && \
 wget https://pjreddie.com/media/files/yolov3.weights && \
-wget https://pjreddie.com/media/files/yolov3-tiny.weights && \
-cp $INSTALL_DIR/deployment_tools/model_optimizer/extensions/front/tf/yolo_v3.json ./ && \
-cp $INSTALL_DIR/deployment_tools/model_optimizer/extensions/front/tf/yolo_v3_tiny.json ./ && \
+wget https://pjreddie.com/media/files/yolov3-tiny.weights
+cp $INSTALL_DIR/deployment_tools/model_optimizer/extensions/front/tf/yolo_v3.json ./
+cp $INSTALL_DIR/deployment_tools/model_optimizer/extensions/front/tf/yolo_v3_tiny.json ./
 
-mkdir -p /home/$2/neural_security_system/models/tiny_yolov3/FP16 && \
-cd /usr/tensorflow-yolo-v3 && \
+mkdir -p /usr/neural_security_system/models/tiny_yolov3/FP16 && \
+cd /usr/tensorflow-yolo-v3
 python3 convert_weights_pb.py --class_names coco.names \
   --data_format NHWC --weights_file yolov3-tiny.weights \
   --output_graph frozen_tiny_yolov3_model.pb --tiny && \
@@ -91,12 +96,12 @@ python3 $INSTALL_DIR/deployment_tools/model_optimizer/mo_tf.py \
   --input_model frozen_tiny_yolov3_model.pb \
   --tensorflow_use_custom_operations_config yolo_v3_tiny.json \
   --input_shape [1,416,416,3] --data_type=FP16 && \
-mv frozen_tiny_yolov3_model.xml /home/$2/neural_security_system/models/tiny_yolov3/FP16/ && \
-mv frozen_tiny_yolov3_model.bin /home/$2/neural_security_system/models/tiny_yolov3/FP16/ && \
-cp coco.names /home/$2/neural_security_system/models/tiny_yolov3/FP16/frozen_tiny_yolov3_model.labels && \
+mv frozen_tiny_yolov3_model.xml /usr/neural_security_system/models/tiny_yolov3/FP16/ && \
+mv frozen_tiny_yolov3_model.bin /usr/neural_security_system/models/tiny_yolov3/FP16/ && \
+cp coco.names /usr/neural_security_system/models/tiny_yolov3/FP16/frozen_tiny_yolov3_model.labels && \
 
-mkdir -p /home/$2/neural_security_system/models/tiny_yolov3/FP32 && \
-cd /usr/tensorflow-yolo-v3 && \
+mkdir -p /usr/neural_security_system/models/tiny_yolov3/FP32 && \
+cd /usr/tensorflow-yolo-v3
 python3 convert_weights_pb.py --class_names coco.names \
   --data_format NHWC --weights_file yolov3-tiny.weights \
   --output_graph frozen_tiny_yolov3_model.pb --tiny && \
@@ -104,12 +109,12 @@ python3 $INSTALL_DIR/deployment_tools/model_optimizer/mo_tf.py \
   --input_model frozen_tiny_yolov3_model.pb \
   --tensorflow_use_custom_operations_config yolo_v3_tiny.json \
   --input_shape [1,416,416,3] && \
-mv frozen_tiny_yolov3_model.xml /home/$2/neural_security_system/models/tiny_yolov3/FP32/ && \
-mv frozen_tiny_yolov3_model.bin /home/$2/neural_security_system/models/tiny_yolov3/FP32/ && \
-cp coco.names /home/$2/neural_security_system/models/tiny_yolov3/FP32/frozen_tiny_yolov3_model.labels && \
+mv frozen_tiny_yolov3_model.xml /usr/neural_security_system/models/tiny_yolov3/FP32/ && \
+mv frozen_tiny_yolov3_model.bin /usr/neural_security_system/models/tiny_yolov3/FP32/ && \
+cp coco.names /usr/neural_security_system/models/tiny_yolov3/FP32/frozen_tiny_yolov3_model.labels && \
 
-mkdir -p /home/$2/neural_security_system/models/yolov3/FP16 && \
-cd /usr/tensorflow-yolo-v3 && \
+mkdir -p /usr/neural_security_system/models/yolov3/FP16 && \
+cd /usr/tensorflow-yolo-v3
 python3 convert_weights_pb.py --class_names coco.names \
   --data_format NHWC --weights_file yolov3.weights \
   --output_graph frozen_yolov3_model.pb && \
@@ -117,12 +122,12 @@ python3 $INSTALL_DIR/deployment_tools/model_optimizer/mo_tf.py \
   --input_model frozen_yolov3_model.pb \
   --tensorflow_use_custom_operations_config yolo_v3.json \
   --input_shape [1,416,416,3] --data_type=FP16 && \
-mv frozen_yolov3_model.xml /home/$2/neural_security_system/models/yolov3/FP16/ && \
-mv frozen_yolov3_model.bin /home/$2/neural_security_system/models/yolov3/FP16/ && \
-cp coco.names /home/$2/neural_security_system/models/yolov3/FP16/frozen_yolov3_model.labels && \
+mv frozen_yolov3_model.xml /usr/neural_security_system/models/yolov3/FP16/ && \
+mv frozen_yolov3_model.bin /usr/neural_security_system/models/yolov3/FP16/ && \
+cp coco.names /usr/neural_security_system/models/yolov3/FP16/frozen_yolov3_model.labels && \
 
-mkdir -p /home/$2/neural_security_system/models/yolov3/FP32 && \
-cd /usr/tensorflow-yolo-v3 && \
+mkdir -p /usr/neural_security_system/models/yolov3/FP32 && \
+cd /usr/tensorflow-yolo-v3
 python3 convert_weights_pb.py --class_names coco.names \
   --data_format NHWC --weights_file yolov3.weights \
   --output_graph frozen_yolov3_model.pb && \
@@ -130,11 +135,11 @@ python3 $INSTALL_DIR/deployment_tools/model_optimizer/mo_tf.py \
   --input_model frozen_yolov3_model.pb \
   --tensorflow_use_custom_operations_config yolo_v3.json \
   --input_shape [1,416,416,3] && \
-mv frozen_yolov3_model.xml /home/$2/neural_security_system/models/yolov3/FP32/ && \
-mv frozen_yolov3_model.bin /home/$2/neural_security_system/models/yolov3/FP32/ && \
-cp coco.names /home/$2/neural_security_system/models/yolov3/FP32/frozen_yolov3_model.labels && \
+mv frozen_yolov3_model.xml /usr/neural_security_system/models/yolov3/FP32/ && \
+mv frozen_yolov3_model.bin /usr/neural_security_system/models/yolov3/FP32/ && \
+cp coco.names /usr/neural_security_system/models/yolov3/FP32/frozen_yolov3_model.labels && \
 
-cd /usr && \
+cd /usr
 rm -rf /usr/tensorflow-yolo-v3
 
 cd /home/$2/
