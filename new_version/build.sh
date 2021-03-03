28e000e992384d8a10972d07d3d92c40997e2f44
Nicholas Page <npage85@gmail.com>
2021-03-02 20:28:29 -0800

    test

--- new_version/build.sh
+++ new_version/build.sh
@@ -14,6 +14,8 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+# test
+
 error() {
     local code="${3:-1}"
     if [[ -n "$2" ]];then
